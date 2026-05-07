99606ba5a887da6ad214d48b6a26c3ada39e3f78
Sihyeon Jeon <login0193@naver.com>
2026-05-08 00:22:48 +0900

    style: apply homepage design

--- src/consts.ts
+++ src/consts.ts
@@ -1,2 +1,2 @@
-export const SITE_TITLE = 'Sihyeon Jeon';
-export const SITE_DESCRIPTION = 'Sihyeon Jeon의 포트폴리오와 기술 블로그';
+export const SITE_TITLE = '전시현';
+export const SITE_DESCRIPTION = '전시현의 포트폴리오와 기술 블로그';
